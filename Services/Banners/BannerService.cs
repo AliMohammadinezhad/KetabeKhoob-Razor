aaf333fc632e23a913bd1598f34e23e1511faa2e
Ali Mohammadinezhad <alimn1382@hotmail.com>
2025-04-12 12:37:20 +0330

    implement banner services

--- Services/Banners/BannerService.cs
+++ Services/Banners/BannerService.cs
@@ -1,6 +1,55 @@
-﻿namespace KetabeKhoob.Razor.Services.Banners;
+﻿using KetabeKhoob.Razor.Models;
+using KetabeKhoob.Razor.Models.Banners;
+
+namespace KetabeKhoob.Razor.Services.Banners;
 
 public class BannerService : IBannerService
 {
+    private readonly HttpClient _httpClient;
 
+    public BannerService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<ApiResult?> CreateBanner(CreateBannerCommand command)
+    {
+        var formData = new MultipartFormDataContent();
+        formData.Add(new StringContent(command.Link), "Link");
+        formData.Add(new StringContent(command.Position.ToString()), "Position");
+        formData.Add(new StreamContent(command.ImageFile.OpenReadStream()), "ImageFile");
+        var result = await _httpClient.PostAsync("banner", formData);
+        return await result.Content.ReadFromJsonAsync<ApiResult>();
+    }
+
+    public async Task<ApiResult?> EditBanner(EditBannerCommand command)
+    {
+        var formData = new MultipartFormDataContent();
+        formData.Add(new StringContent(command.Id.ToString()), "Id");
+        formData.Add(new StringContent(command.Link), "Link");
+        formData.Add(new StringContent(command.Position.ToString()), "Position");
+        formData.Add(new StreamContent(command.ImageFile.OpenReadStream()), "ImageFile");
+        var result = await _httpClient.PutAsync("banner", formData);
+        return await result.Content.ReadFromJsonAsync<ApiResult>();
+    }
+
+    public async Task<ApiResult?> DeleteBanner(long bannerId)
+    {
+        var result = await _httpClient.DeleteAsync($"banner/{bannerId}");
+        return await result.Content.ReadFromJsonAsync<ApiResult>();
+    }
+
+    public async Task<BannerDto?> GetBannerById(long bannerId)
+    {
+        var result = await _httpClient.GetFromJsonAsync<ApiResult<BannerDto>>($"banner/{bannerId}");
+        return result?.Data;
+    }
+
+    public async Task<List<BannerDto>> GetBannerList()
+    {
+        var result = await _httpClient.GetFromJsonAsync<ApiResult<List<BannerDto>>>("banner");
+        if (result?.Data is null)
+            return [];
+        return result.Data;
+    }
 }
--- Services/Banners/IBannerService.cs
+++ Services/Banners/IBannerService.cs
@@ -1,6 +1,14 @@
-﻿namespace KetabeKhoob.Razor.Services.Banners;
+﻿using KetabeKhoob.Razor.Models;
+using KetabeKhoob.Razor.Models.Banners;
+
+namespace KetabeKhoob.Razor.Services.Banners;
 
 public interface IBannerService
 {
-    
+    Task<ApiResult?> CreateBanner(CreateBannerCommand command);
+    Task<ApiResult?> EditBanner(EditBannerCommand command);
+    Task<ApiResult?> DeleteBanner(long bannerId);
+
+    Task<BannerDto?> GetBannerById(long bannerId);
+    Task<List<BannerDto>> GetBannerList();
 }
