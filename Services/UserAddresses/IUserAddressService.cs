cc2cb35505f1584a1b92587129604ed1e55d5e99
Ali Mohammadinezhad <alimn1382@hotmail.com>
2025-04-30 14:02:05 +0330

    implement UserAddresses Models and services

--- Services/UserAddresses/IUserAddressService.cs
+++ Services/UserAddresses/IUserAddressService.cs
@@ -1,6 +1,14 @@
-﻿namespace KetabeKhoob.Razor.Services.UserAddresses;
+﻿using KetabeKhoob.Razor.Models;
+using KetabeKhoob.Razor.Models.UserAddresses;
+
+namespace KetabeKhoob.Razor.Services.UserAddresses;
 
 public interface IUserAddressService
 {
-    
+    Task<ApiResult> CreateAddress(CreateUserAddressCommand command);
+    Task<ApiResult> EditAddress(EditUserAddressCommand command);
+    Task<ApiResult> DeleteAddress(long addressId);
+
+    Task<AddressDto?> GetAddressById(long id);
+    Task<List<AddressDto?>> GetAddresses();
 }
--- Services/UserAddresses/UserAddressService.cs
+++ Services/UserAddresses/UserAddressService.cs
@@ -1,6 +1,46 @@
-﻿namespace KetabeKhoob.Razor.Services.UserAddresses;
+﻿using KetabeKhoob.Razor.Models;
+using KetabeKhoob.Razor.Models.UserAddresses;
+
+namespace KetabeKhoob.Razor.Services.UserAddresses;
 
 public class UserAddressService : IUserAddressService
 {
+    private readonly HttpClient _httpClient;
 
+    private const string ModuleName = "UserAddress";
+
+    public UserAddressService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<ApiResult> CreateAddress(CreateUserAddressCommand command)
+    {
+        var result = await _httpClient.PostAsJsonAsync(ModuleName, command);
+        return await result.Content.ReadFromJsonAsync<ApiResult>();
+    }
+
+    public async Task<ApiResult> EditAddress(EditUserAddressCommand command)
+    {
+        var result = await _httpClient.PutAsJsonAsync(ModuleName, command);
+        return await result.Content.ReadFromJsonAsync<ApiResult>();
+    }
+
+    public async Task<ApiResult> DeleteAddress(long addressId)
+    {
+        var result = await _httpClient.DeleteAsync($"{ModuleName}/{addressId}");
+        return await result.Content.ReadFromJsonAsync<ApiResult>();
+    }
+
+    public async Task<AddressDto?> GetAddressById(long id)
+    {
+        var result = await _httpClient.GetFromJsonAsync<ApiResult<AddressDto?>>($"{ModuleName}/{id}");
+        return result?.Data;
+    }
+
+    public async Task<List<AddressDto?>> GetAddresses()
+    {
+        var result = await _httpClient.GetFromJsonAsync<ApiResult<List<AddressDto?>>>(ModuleName);
+        return result?.Data;
+    }
 }
