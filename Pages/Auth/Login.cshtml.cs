2950f05a4ebcd9152227a0db13ceb4026eb7636e
Ali Mohammadinezhad <alimn1382@hotmail.com>
2025-05-06 22:57:58 +0330

    complete auth pages and create EditorTemplates

--- Pages/Auth/Login.cshtml.cs
+++ Pages/Auth/Login.cshtml.cs
@@ -1,12 +1,57 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using KetabeKhoob.Razor.Models.Auth;
+using KetabeKhoob.Razor.Services.Auth;
 
 namespace KetabeKhoob.Razor.Pages.Auth
 {
+    [BindProperties]
+    [ValidateAntiForgeryToken]
     public class LoginModel : PageModel
     {
-        public void OnGet()
+        [Display(Name = "شماره تلفن")]
+        [Required(ErrorMessage = "{0} را وارد کنید.")]
+        [MinLength(5, ErrorMessage = "کلمه عبور باید بزرگتر از 5 کاراکتر باشد.")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "{0} را وارد کنید.")]
+        [DataType(DataType.Password)]
+        [MinLength(5, ErrorMessage = "کلمه عبور باید بزرگتر از 5 کاراکتر باشد.")]
+        public string Password { get; set; }
+
+        private readonly IAuthService _authService;
+        public LoginModel(IAuthService authService)
         {
+            _authService = authService;
+        }
+
+        public Task<IActionResult> OnGet()
+        {
+            if(User.Identity.IsAuthenticated)
+                return Task.FromResult<IActionResult>(Redirect("/"));
+            return Task.FromResult<IActionResult>(Page());
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var result = await _authService.Login(new LoginCommand()
+            {
+                PhoneNumber = PhoneNumber,
+                Password = Password
+            });
+            if (result.IsSuccess is false)
+            {
+                ModelState.AddModelError(nameof(PhoneNumber), result.MetaData.Message);
+                return Page();
+            }
+
+            var accessToken = result.Data.AccessToken;
+            var refreshToken = result.Data.RefreshToken;
+            HttpContext.Response.Cookies.Append("access-token", accessToken);
+            HttpContext.Response.Cookies.Append("refresh-token", refreshToken);
+            return Redirect("/");
         }
     }
 }
--- Pages/Auth/Register.cshtml.cs
+++ Pages/Auth/Register.cshtml.cs
@@ -1,12 +1,57 @@
+using System.ComponentModel.DataAnnotations;
+using KetabeKhoob.Razor.Models.Auth;
+using KetabeKhoob.Razor.Services.Auth;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace KetabeKhoob.Razor.Pages.Auth
 {
+    [BindProperties]
+    [ValidateAntiForgeryToken]
     public class RegisterModel : PageModel
     {
+        [Display(Name = "شماره تلفن")]
+        [Required(ErrorMessage = "{0} را وارد کنید.")]
+        [MinLength(5, ErrorMessage = "کلمه عبور باید بزرگتر از 5 کاراکتر باشد.")]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "{0} را وارد کنید.")]
+        [MinLength(5, ErrorMessage = "کلمه عبور باید بزرگتر از 5 کاراکتر باشد.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "تکرار کلمه عبور")]
+        [Compare(nameof(Password), ErrorMessage = "کلمات عبور یکسان نیستند.")]
+        [Required(ErrorMessage = "{0} را وارد کنید.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        private readonly IAuthService _authService;
+
+        public RegisterModel(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var result = await _authService.Register(new RegisterCommand()
+            {
+                PhoneNumber = PhoneNumber,
+                Password = Password,
+                ConfirmPassword = ConfirmPassword
+            });
+
+            if (result?.IsSuccess is false)
+            {
+                ModelState.AddModelError(nameof(PhoneNumber), result.MetaData.Message);
+                return Page();
+            }
+
+            return RedirectToPage("Login");
         }
     }
 }
